3e2e47a3bf7c821a9390998fde59a9cd9b925bd1
Caie Hallite <hallitecaie@gmail.com>
2025-04-08 12:33:17 -0300

    4th Commit - Resolving Grid problem and Adding bottomBarNavigation

--- app/src/main/java/com/example/basiclayoutdocumentation/MainActivity.kt
+++ app/src/main/java/com/example/basiclayoutdocumentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
+import androidx.annotation.NavigationRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -22,6 +23,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -30,14 +32,20 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Spa
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -56,6 +64,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toDrawable
 import com.example.basiclayoutdocumentation.ui.theme.BasicLayoutDocumentationTheme
+import kotlinx.coroutines.selects.select
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,16 +72,63 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BasicLayoutDocumentationTheme {
-                FavoriteGrid()
+                MyAppPortrait()
             }
         }
     }
 }
 
+@Composable
+private fun MyAppPortrait(){
+    BasicLayoutDocumentationTheme {
+        Scaffold (
+            bottomBar = { BottomNavigation() }
+        ){ padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
+
+@Composable
+private fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(modifier.verticalScroll(rememberScrollState()))
+    {
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            RowAlign()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun HomeSection(
+    title: Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+        )
+        content()
+    }
+}
 
 
 @Composable
-fun SearchBar(modifier: Modifier = Modifier) {
+private fun SearchBar(modifier: Modifier = Modifier) {
     TextField(
         value = "",
         onValueChange = {},
@@ -97,7 +153,7 @@ fun SearchBar(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun RowAlign(modifier: Modifier = Modifier) {
+private fun RowAlign(modifier: Modifier = Modifier) {
 
     LazyRow(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
@@ -110,7 +166,7 @@ fun RowAlign(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun AlignYourBodyElement(
+private fun AlignYourBodyElement(
     @DrawableRes drawable: Int,
     @StringRes text: Int,
     modifier: Modifier = Modifier
@@ -138,24 +194,24 @@ fun AlignYourBodyElement(
 }
 
 @Composable
-fun FavoriteGrid(modifier: Modifier = Modifier){
+private fun FavoriteGrid(
+    modifier: Modifier = Modifier
+) {
     LazyHorizontalGrid(
         rows = GridCells.Fixed(2),
         contentPadding = PaddingValues(horizontal = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = modifier
-    ){
-        items(favoriteCollectionsData){ item ->
-            FavoriteCollection(
-                item.drawable, item.text,
-                modifier.height(80.dp))
+        modifier = modifier.height(168.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollection(item.drawable, item.text, Modifier.height(80.dp))
         }
     }
 }
 
 @Composable
-fun FavoriteCollection(
+private fun FavoriteCollection(
     @DrawableRes drawable: Int,
     @StringRes text: Int,
     modifier: Modifier = Modifier
@@ -167,14 +223,16 @@ fun FavoriteCollection(
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.width(255.dp)
-                .height(IntrinsicSize.Min)
+            modifier = Modifier
+                .width(255.dp)
+                .height(80.dp)
         ) {
             Image(
                 painter = painterResource(drawable),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.size(80.dp)
+                modifier = Modifier
+                    .size(80.dp)
                     .clip(
                         RoundedCornerShape(
                             topStart = 12.dp,
@@ -191,10 +249,50 @@ fun FavoriteCollection(
     }
 }
 
+@Composable
+private fun BottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Spa,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    stringResource(R.string.bottom_navigation_home)
+                )
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.bottom_navigation_profile)
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
+
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
-fun GreetingPreview() {
+fun PreviewHome() {
     BasicLayoutDocumentationTheme {
-        FavoriteGrid()
+        MyAppPortrait()
     }
 }
